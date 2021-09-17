7afd465cd5c5ed4ec2739854be6d731cf78f5853
Sergey Olshanov <sergeyolshanov@gmail.com>
2021-09-17 15:41:56 +0500

    Create new notes in Firebase

--- app/src/main/java/com/example/notes/sergeyolshanovapp/module/app_notes/database/firebase/AppNoteFirebaseRepository.kt
+++ app/src/main/java/com/example/notes/sergeyolshanovapp/module/app_notes/database/firebase/AppNoteFirebaseRepository.kt
@@ -3,18 +3,29 @@ package com.example.notes.sergeyolshanovapp.module.app_notes.database.firebase
 import androidx.lifecycle.LiveData
 import com.example.notes.sergeyolshanovapp.module.app_notes.database.DatabaseNoteRepository
 import com.example.notes.sergeyolshanovapp.module.app_notes.model.AppNote
-import com.example.notes.utilits.EMAIL
-import com.example.notes.utilits.PASSWORD
+import com.example.notes.utilits.*
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class AppNoteFirebaseRepository : DatabaseNoteRepository {
 
     private val mAuth = FirebaseAuth.getInstance()
+    private val mDatabaseNoteRepository = FirebaseDatabase.getInstance().reference
+        .child(mAuth.currentUser?.uid.toString())
 
     override val allNotes: LiveData<List<AppNote>> = AllNotesLiveData()
 
     override suspend fun insert(note: AppNote, onSuccess: () -> Unit) {
+        val idNote = mDatabaseNoteRepository.push().key.toString()
+        val mapNote = hashMapOf<String, Any>()
+        mapNote[ID_FIREBASE] = idNote
+        mapNote[NAME] = note.name
+        mapNote[TEXT] = note.text
 
+        mDatabaseNoteRepository.child(idNote)
+            .updateChildren(mapNote)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { showToast(it.message.toString()) }
     }
 
     override suspend fun delete(note: AppNote, onSuccess: () -> Unit) {
